1fd7f3d7b00291327dbaed207f94cb8cadd07563
Garbo Alan <39155304+NalaGarbo@users.noreply.github.com>
2021-03-12 01:40:47 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 def get_tokens(serviceName):
-    telegramToken = '1610469297:AAEXrA5Fc-_sv3X2Qk44s9mnx8-JKxOS5k4'
-    navitiaToken = '1592879d-5a2c-4727-8932-9a2f0fe64b17'
+    telegramToken = 'your token
+    navitiaToken = 'your token'
 
     if serviceName == 'telegram':
         return telegramToken
